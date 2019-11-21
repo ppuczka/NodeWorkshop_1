d7f0409b3abeddef2426e846472e6e50d90657ee
Przemyslaw Puczka <p.puczka@gmail.com>
2019-11-21 13:51:52 +0100

    node.js Workshop

---
+++ app.js
@@ -0,0 +1,6 @@
+const http = require('http');
+
+const requestHadler = require('./routes.js')
+
+const server = http.createServer(requestHadler);
+    server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,64 @@
+
+
+const userList = [];
+
+const requestHandler = ((req, res) => {
+    const url = req.url;
+    const method = req.method;
+   
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write(
+          '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+        );
+        res.write('</html>');
+        return res.end();
+      }
+      if (url === '/users') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        userList.forEach(u => console.log(u));
+        return res.end();
+      }
+      if (url === '/create-user') {
+        const body = [];
+        req.on('data', formData => {
+          body.push(formData);
+        });
+        req.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
+          userList.push(parsedBody.split('=')[1]);
+
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.end();
+      }
+    // if (url === "/create-user" && method === 'POST') {
+    //     const body = [];
+    //     req.on('formData', data => {
+    //         body.push(data);
+    //     });
+    //     console.log('not parsed' + body.toString);
+    //     req.on('end', () => {
+    //         const parsedBody = Buffer.concat(body).toString();
+    //         console.log("ParsedBody" + parsedBody);
+    //         const userName  = parsedBody.split(('=')[1]);
+    //         console.log("UserName " + userName)
+    //         userList.push(userName);
+           
+    //     });
+    //     res.statusCode = 302; 
+    //     res.setHeader('Location', '/');
+    //     res.end();
+    // }
+
+});
+
+module.exports = requestHandler;
